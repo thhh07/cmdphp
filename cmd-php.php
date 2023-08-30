6985c9d8654f1a1dcf4e98b1c84f048cdebbd1dc
Arthur Rocha Gomes <arthur.gomes@tc.df.gov.br>
2023-08-30 13:42:25 -0300

    1

---
+++ cmd-php.php
@@ -0,0 +1,85 @@
+<?php 
+/*
+COMANDOS PHP
+
+VARIÁVEIS----------------------------------------
+ INICIAM COM "$"
+ VARIÁVEL PODE SER IMPRESSA NA TELA ATRAVEZ DO "ECHO"  DENTRO DE ASPAS DUPLAS
+
+FLOATS-------------------------------------------
+ CASAS DECIMAIS
+ >EX:
+ >is_float() = testar se dado inserido e float
+
+STRINGS-----------------------------------------
+ TEXTOS 
+ TEXTOS SÃO INSERIDOS EM ASPAS SIMPLES OU DUPLAS
+
+BOOLEANOS---------------------------------------
+ VERDADEIRO - TRUE
+ FALSO - FALSE
+
+ARRAYS 
+ CONJUNTO DE LISTAS-----------------------------
+ ARRAY COM VÁRIOS ITENS 
+ >EX:
+ >$itens = ["teto solar", "Motor", "Porta];
+
+OPERADORES PHP--------------------------------
+ ( == ) IGUAL
+ ( != ) DIFERENTE
+ ( === ) IDÊNTICO
+ ( !== ) NÃO IDÊNTICO 
+ ( > ) MAIOR 
+ ( < ) MENOR 
+ ( <= ) MENOR OU IGUAL
+ ( ? ) OPERADOR TERNÁRIO
+
+OPRADORES LÓGICOS--------------------------- 
+ ( && ) CHECA AS DUAS OPERAÇÕES AO MESMO TEMPO / AS DUAS  TEM Q SER VERDADEIRAS >OPERADOR AND
+ ( || ) CHECA AS OPERAÇÕES AO MESMO TEMPO JÁ BASTA UMA  OPERAÇÃO SER VERDADEIRA > OPERADOR OR
+ -APENAS UM LADO PRECISA FUNCIONAR
+ ( ! ) RETORNA O CONTRÁRIO DO RESULTADO DA OPERAÇÃO
+
+OPERADORES DE CONVERSÃO
+
+OPERADOR TERNÁRIO--------------------------
+ >EX:
+ >echo 20 > 10 ? "Deu true <br>" : "Deu false";
+ >resultado = Deu true
+
+ESTRUTURA IF-------------------------------
+ CHECA SE A EXPRESSÃO E VERDADEIRA
+ >EX:
+ > if(5 > 2){
+ >deu certo
+ >}
+ >resultado = deu certo
+
+SWITCH-----------------------------------
+ PODEMOS ADICIONAR A INSTRUÇÃO BREAK, PARA NÃO 
+ SER MAIS EXECUTADO
+
+IF/ ELSE --------------------------------
+ executado um de cada vez
+ >EX:
+ >if("teste" === 5) {
+ >echo "entrou no if";
+ >} else { echo "entrou no else";
+ >}
+ >resultado = 
+
+
+~~~~~~~~~~~~~~
+> EX: IRA IMPRIMIR NA TELA "DIFERENTE"
+> POIS AS VARIÁVEIS NN SÃO IGUAIS 
+> ( != ) DIFERENTE
+
+> $a = 8;
+> $b = 5; 
+>  if($a != $b){
+>      echo "diferente<br>";
+>    }
+~~~~~~~~~~~~~~~~
+*/
+?>
